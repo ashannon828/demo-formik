85414ad287556310cd4494c3c6911874eddf51ad
ashannon828 <ashannon828@gmail.com>
2021-02-22 22:56:31 +0200

    got form set up

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
 import Head from "next/head";
-import { Box, Heading, Text } from "@chakra-ui/react";
-import { useFormik } from "formik";
+import {
+  Box,
+  Button,
+  Heading,
+  Input,
+  Text,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from "@chakra-ui/react";
+import React from "react";
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
 
 export default function Home() {
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-    },
-
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
   return (
     <Box>
       <Head>
@@ -19,25 +22,83 @@ export default function Home() {
       </Head>
 
       <Box as="main">
-        <Box>
-          <Heading as="h1">React forms with Formik and Yup</Heading>
-          <Text>
-            Simple demo of forms with react using Formik and Yup validation.
-          </Text>
+        <Box p="1rem">
+          <Box>
+            <Heading as="h1">React forms with Formik and Yup</Heading>
+            <Text>
+              Simple demo of forms with react using Formik and Yup validation.
+            </Text>
+          </Box>
+          <Formik
+            initialValues={{ firstName: "", lastName: "", email: "" }}
+            validationSchema={Yup.object({
+              firstName: Yup.string()
+
+                .max(15, "Must be 15 characters or less")
+
+                .required("Required"),
+
+              lastName: Yup.string()
+
+                .max(20, "Must be 20 characters or less")
+
+                .required("Required"),
+
+              email: Yup.string()
+                .email("Invalid email address")
+                .required("Required"),
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {(formik) => (
+              <Form>
+                <FormLabel htmlFor="firstName">First Name</FormLabel>
+
+                <Input
+                  id="firstName"
+                  type="text"
+                  {...formik.getFieldProps("firstName")}
+                />
+
+                {formik.touched.firstName && formik.errors.firstName ? (
+                  <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
+                ) : null}
+
+                <FormLabel htmlFor="lastName">Last Name</FormLabel>
+
+                <Input
+                  id="lastName"
+                  type="text"
+                  {...formik.getFieldProps("lastName")}
+                />
+
+                {formik.touched.lastName && formik.errors.lastName ? (
+                  <FormErrorMessage>{formik.errors.lastName}</FormErrorMessage>
+                ) : null}
+
+                <FormLabel htmlFor="email">Email Address</FormLabel>
+
+                <Input
+                  id="email"
+                  type="email"
+                  {...formik.getFieldProps("email")}
+                />
+
+                {formik.touched.email && formik.errors.email ? (
+                  <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+                ) : null}
+
+                <Button type="submit">Submit</Button>
+              </Form>
+            )}
+          </Formik>
         </Box>
-        <form onSubmit={formik.handleSubmit}>
-          <label htmlFor="email">Email Address</label>
-
-          <input
-            id="email"
-            name="email"
-            type="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-          />
-
-          <button type="submit">Submit</button>
-        </form>
       </Box>
     </Box>
   );
