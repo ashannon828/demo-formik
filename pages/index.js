72b5a639ff79e218e6584765e1b6d51e75f6cb8e
ashannon828 <ashannon828@gmail.com>
2021-03-27 16:48:54 +0200

    finished errors for form validation demo

--- pages/index.js
+++ pages/index.js
@@ -36,76 +36,108 @@ export default function Home() {
             maxW="400px"
           >
             <Box>
-              <Heading as="h1">React forms with Formik and Yup</Heading>
-              <Text>
-                Simple demo of forms with react using Formik and Yup validation.
-              </Text>
+              <Heading as="h1">
+                Validating forms with React using Formik and Yup
+              </Heading>
             </Box>
             <Formik
-              initialValues={{ firstName: "", lastName: "", email: "" }}
+              initialValues={{
+                firstName: "",
+                lastName: "",
+                email: "",
+                confirmEmail: "",
+              }}
               validationSchema={Yup.object({
                 firstName: Yup.string()
                   .max(15, "Must be 15 characters or less")
-                  .required("Required"),
+                  .required("Enter your first name"),
                 lastName: Yup.string()
                   .max(20, "Must be 20 characters or less")
-                  .required("Required"),
+                  .required("Enter your last name"),
                 email: Yup.string()
-                  .email("Invalid email address")
-                  .required("Required"),
+                  .email("Enter a valid email")
+                  .required("Enter your email"),
+                confirmEmail: Yup.string()
+                  .email("Enter a valid email")
+                  .required("Please confirm your email"),
               })}
               onSubmit={(values, { setSubmitting }) => {
                 setTimeout(() => {
                   alert(JSON.stringify(values, null, 2));
-
                   setSubmitting(false);
                 }, 400);
               }}
             >
               {(formik) => (
                 <Form>
-                  <FormLabel mt="2rem" htmlFor="firstName">
-                    First Name
-                  </FormLabel>
-                  <Input
-                    id="firstName"
-                    type="text"
-                    {...formik.getFieldProps("firstName")}
-                  />
-
-                  {formik.touched.firstName && formik.errors.firstName ? (
+                  <FormControl
+                    isInvalid={
+                      formik.touched.firstName && formik.errors.firstName
+                    }
+                  >
+                    <FormLabel mt="2rem" htmlFor="firstName">
+                      First Name*
+                    </FormLabel>
+                    <Input
+                      id="firstName"
+                      type="text"
+                      {...formik.getFieldProps("firstName")}
+                    />
                     <FormErrorMessage>
                       {formik.errors.firstName}
                     </FormErrorMessage>
-                  ) : null}
+                  </FormControl>
 
-                  <FormLabel mt="1rem" htmlFor="lastName">
-                    Last Name
-                  </FormLabel>
-                  <Input
-                    id="lastName"
-                    type="text"
-                    {...formik.getFieldProps("lastName")}
-                  />
-
-                  {formik.touched.lastName && formik.errors.lastName ? (
+                  <FormControl
+                    isInvalid={
+                      formik.touched.lastName && formik.errors.lastName
+                    }
+                  >
+                    <FormLabel mt="1rem" htmlFor="lastName">
+                      Last Name*
+                    </FormLabel>
+                    <Input
+                      id="lastName"
+                      type="text"
+                      {...formik.getFieldProps("lastName")}
+                    />
                     <FormErrorMessage>
                       {formik.errors.lastName}
                     </FormErrorMessage>
-                  ) : null}
+                  </FormControl>
 
-                  <FormLabel mt="1rem" htmlFor="email">
-                    Email Address
-                  </FormLabel>
-                  <Input
-                    id="email"
-                    type="email"
-                    {...formik.getFieldProps("email")}
-                  />
-
-                  {formik.touched.email && formik.errors.email ? (
+                  <FormControl
+                    isInvalid={formik.touched.email && formik.errors.email}
+                  >
+                    <FormLabel mt="1rem" htmlFor="email">
+                      Email Address*
+                    </FormLabel>
+                    <Input
+                      id="email"
+                      type="email"
+                      {...formik.getFieldProps("email")}
+                    />
                     <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
-                  ) : null}
+                  </FormControl>
+
+                  <FormControl
+                    isInvalid={
+                      formik.touched.confirmEmail && formik.errors.confirmEmail
+                    }
+                  >
+                    <FormLabel mt="1rem" htmlFor="confirmEmail">
+                      Confirm Email
+                    </FormLabel>
+                    <Input
+                      id="confirmEmail"
+                      type="confirmEmail"
+                      {...formik.getFieldProps("confirmEmail")}
+                    />
+                    <FormErrorMessage>
+                      {formik.errors.confirmEmail}
+                    </FormErrorMessage>
+                  </FormControl>
+
                   <Center mt="1rem">
                     <Button type="submit">Submit</Button>
                   </Center>
